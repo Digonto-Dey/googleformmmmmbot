183f7b7f05ce577e45e1b7a5e47a3ec7b82608a4
Digonto Dey <deydigonto@gmail.com>
2026-05-04 01:52:27 +0600

    2nd edition

--- get_page_history.py
+++
@@ -1,4 +0,0 @@
-import requests, re
-res=requests.get('https://docs.google.com/forms/d/e/1FAIpQLSe-2fx_ibOrVF-SIuf6tD5mwAHU_-5HKono73i-i1Sy44fEDw/viewform')
-matches = re.findall(r'name="pageHistory" value="(.*?)"', res.text)
-print("pageHistory:", matches)
